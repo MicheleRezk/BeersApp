cf3b98a6f8ec32622739d21edf1a1085fd74f911
Michele Rezk <michosoft@hotmail.com>
2018-04-18 18:21:17 +0200

    Add Beer Model

---
+++ BeersApp/BeersApp.Domain/Beer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BeersApp.Domain
+{
+    public class Beer
+    {
+        #region Properites
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string NameDisplay { get; set; }
+        public string Description { get; set; }
+        #endregion
+    }
+}
